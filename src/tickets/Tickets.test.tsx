18ebb9a780ec234dd00e67e85b598aee8b900a72
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-09 00:03:01 +0300

    начал писать тесты на отображение тикетов

---
+++ src/tickets/Tickets.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent, getByAltText } from "@testing-library/react";
+import Tickets from "./Tickets";
+
+import { TicketType } from "../data/ticketsTypes";
+
+describe("Tickets", () => {
+  it("should render with default sorting", () => {
+    // TODO: тесты на отображение тикетов
+  });
+});
