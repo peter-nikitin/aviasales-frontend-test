f9392ecb95b7728925589367498ac2d1ba598882
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-12 21:52:43 +0300

    Updates

---
+++ precache-manifest.04b03c929e11be1ba7ea8b5e26de2d7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc9f2e5fe1948fea906f53ac91f8bf6",
+    "url": "/aviasales-frontend-test/index.html"
+  },
+  {
+    "revision": "f1d5f01b7dff5eb9da48",
+    "url": "/aviasales-frontend-test/static/css/main.246844d0.chunk.css"
+  },
+  {
+    "revision": "18d53d28ce99a5b8055a",
+    "url": "/aviasales-frontend-test/static/js/2.9bc29093.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/aviasales-frontend-test/static/js/2.9bc29093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1d5f01b7dff5eb9da48",
+    "url": "/aviasales-frontend-test/static/js/main.bc512442.chunk.js"
+  },
+  {
+    "revision": "8c19273190fd9fb10389",
+    "url": "/aviasales-frontend-test/static/js/runtime-main.838cec6a.js"
+  },
+  {
+    "revision": "0c08923414ada761c4d56d4927f4d1d9",
+    "url": "/aviasales-frontend-test/static/media/Logo.0c089234.svg"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/aviasales-frontend-test/static/media/selected.68ca57dc.svg"
+  }
+]);
