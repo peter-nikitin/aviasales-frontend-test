b41d7cf337f7f5cca7a256b00bc40b40758d374d
Petr Nikitin <nikitin.p.94@gmail.com>
2020-10-12 21:34:19 +0300

    убрал лишнее объявление типа

--- src/utils/api/getSearchId.ts
+++ src/utils/api/getSearchId.ts
@@ -4,10 +4,9 @@ import { SearchIdType } from "../../data/types.d";
 
 const getSearchId = async () => {
   try {
-    const searchId: Error | AxiosResponse<SearchIdType> = await axios.get<
-      string,
-      AxiosResponse<SearchIdType>
-    >(`https://front-test.beta.aviasales.ru/search`);
+    const searchId: AxiosResponse<SearchIdType> = await axios.get(
+      `https://front-test.beta.aviasales.ru/search`
+    );
     return searchId;
   } catch (error) {
     return new Error(error);
